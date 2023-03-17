c045c936ec87ddf4adfa5e7fba18a7b8d8a671ce
Arakakija <48024442+Arakakija@users.noreply.github.com>
2023-03-17 04:36:31 -0300

    Added Player Animation Walk

--- BlueGravity/Assets/Scripts/Player/PlayerStates/PlayerNormalState.cs
+++ BlueGravity/Assets/Scripts/Player/PlayerStates/PlayerNormalState.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class PlayerNormalState : PlayerBaseState
 {
+    private static readonly int Speed = Animator.StringToHash("Speed");
+    private static readonly int Mirror = Animator.StringToHash("Mirror");
+    private const float AnimatorDampTime = 0.1f;
+    private const float FixedTransitionDuration = 0.1f;
     public PlayerNormalState(PlayerStateMachine stateMachine) : base(stateMachine)
     {
     }
@@ -18,11 +22,30 @@ public class PlayerNormalState : PlayerBaseState
     {
         Vector2 movement = CalculateMovement();
         Move(movement, DeltaTime);
+
+        if (_stateMachine.InputReader.MovementValue == Vector2.zero)
+        {
+            _stateMachine.Animator.SetFloat(Speed,0,AnimatorDampTime,FixedTransitionDuration);
+            return;
+        }
+        
+        if(_stateMachine.InputReader.MovementValue.x != 0)
+            TryMirrorPlayer();
+        
+        _stateMachine.Animator.SetFloat(Speed,1,AnimatorDampTime,FixedTransitionDuration);
+        
     }
 
     public override void Exit()
     {
         
+    }
+
+    void TryMirrorPlayer()
+    {
+        AnimatorStateInfo stateInfo = _stateMachine.Animator.GetCurrentAnimatorStateInfo(0);
+        bool mirror = stateInfo.IsName("Normal State") && _stateMachine.InputReader.MovementValue.x > 0.5f;
+       _stateMachine.transform.localScale = new Vector3(mirror ? -1 : 1, 1, 1);
     }
 
     Vector2 CalculateMovement()
